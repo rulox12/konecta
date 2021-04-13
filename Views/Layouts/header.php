0460869369de4c40ba5fceec380e8bd9b3e51a8d
Daniel Betancur <daniel.betancur@evertecinc.com>
2021-04-13 18:54:02 -0500

    feat: the necessary views are added to see edit create and delete a product

---
+++ Views/Layouts/header.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link btn btn-primary" href="?controller=product&action=register">Registrar</a>
+        </li>
+        <br/>
+        <li class="nav-item">
+            <a class="nav-link btn btn-primary" href="?controller=product&&action=show">Ver</a>
+        </li>
+    </ul>
+</div>
